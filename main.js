f8f8e2edf5b6821f5cf4e0f83867e7af0d60f667
Bruno Canini <brunocanini1994@gmail.com>
2023-05-04 16:33:26 +0200

    bud footer

--- main.js
+++ main.js
@@ -62,13 +62,76 @@ for (let i = 0; i < posts.length; i++) {
     const element = posts[i];
 
     let containerPost = document.createElement("div")
-    containerPost.classList.add("post")
+    containerPost.classList.add("post");
     containerGeneral.append(containerPost);
 
+    // creazione post header
     let postHeaderJs = document.createElement("div")
     postHeaderJs.classList.add("post__header");
     containerPost.append(postHeaderJs);
 
-    let 
-    
+    let childHeader = document.createElement("div")
+    childHeader.classList.add("post-meta__icon");
+    postHeaderJs.append(childHeader);
+
+    let image = document.createElement("img")
+    image.src = `${element.author.image}`
+    image.classList.add("profile-pic");
+    childHeader.append(image);
+
+    let secondChildHeader = document.createElement("div");
+    secondChildHeader.classList.add("post-meta__data");
+    childHeader.append(secondChildHeader);
+
+    let childSecondChildHeader = document.createElement("div");
+    childSecondChildHeader.classList.add("post-meta__author");
+    childSecondChildHeader.textContent = `${element.author.name}`;
+    secondChildHeader.append(childSecondChildHeader);
+
+    let secondChildSecondChildHeader = document.createElement("div");
+    secondChildSecondChildHeader.classList.add("post-meta__time");
+    secondChildSecondChildHeader.textContent = `${element.created}`;
+    secondChildHeader.append(secondChildSecondChildHeader);
+
+    // creazione post text
+    let postTextJs = document.createElement("div");
+    postTextJs.classList.add("post__text");
+    postTextJs.textContent = `${element.content}`;
+    containerPost.append(postTextJs);
+
+    // creazione post image
+    let containerImage = document.createElement("div");
+    containerImage.classList.add("post__image");
+    containerPost.append(containerImage);
+
+    let postImage = document.createElement("img");
+    postImage.src = `${element.media}`;
+    containerImage.append(postImage);
+
+    // creazione post footer
+    // const containerFooterJs = document.createElement("div");
+    // containerFooterJs.classList.add("post__footer");
+    // containerPost.append(containerFooterJs);
+
+    // const childFooter = document.createElement("div");
+    // childFooter.classList.add("likes js-likes");
+    // containerFooterJs.append(childFooter);
+
+    // const childChildFooter = document.createElement("div");
+    // childChildFooter.classList.add("likes__cta");
+    // childFooter.append(childChildFooter);
+
+    // const linkFooter = document.createElement("a");
+    // linkFooter.classList.add("like-button");
+    // linkFooter.classList.add("js-like-button");
+    // childChildFooter.append(linkFooter);
+
+    // const picLike = document.createElement("i");
+    // picLike.classList.add("like-button__icon");
+    // picLike.classList.add("fas");
+    // picLike.classList.add("fa-thumbs-up");
+    // linkFooter.append(picLike);
+
+
+
 }
